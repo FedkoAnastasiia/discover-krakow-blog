974021ddbe10d3078b960ec20ab0884b1afc8995
Anastasia Fedko <fedko@Anastasias-MacBook-Pro.local>
2024-09-25 10:53:55 +0200

    event description dynamic import

--- components/EventPost/index.tsx
+++ components/EventPost/index.tsx
@@ -1,7 +1,14 @@
-import "./index.css";
+import "./index.scss";
 
-const EventPost = () => {
-  return <></>;
+interface IEventPostProps {
+  description: string;
+}
+
+const EventPost = ({ description }: IEventPostProps) => {
+  const eventDescription = document.createElement("div");
+  eventDescription.innerHTML = description;
+  eventDescription.className = "eventDescription";
+  return eventDescription;
 };
 
 export default EventPost;
